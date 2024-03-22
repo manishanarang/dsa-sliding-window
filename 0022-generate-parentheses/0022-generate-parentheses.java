0d24b302a5c3f9676657ecd9b8e03c4314cc2a39
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-03-23 01:56:21 +0530

    Time: 1 ms (84.96%), Space: 43.3 MB (30.02%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,29 +2,19 @@ class Solution {
     List<String> result = new ArrayList<>();
     
     public List<String> generateParenthesis(int n) {
-        Deque<Character> dq = new LinkedList<>();
-        backtrack(0,0,n, dq);
+        backtrack(0,0,"", n);
         return result;
     }
     
-    public void backtrack(int open, int close,int n, Deque<Character> dq){
+    public void backtrack(int open, int close,String str, int n){
         if(open == n && close == n){
-            String s = "";
-            Iterator it = dq.iterator();
-            while(it.hasNext()){
-                s+= it.next();
-            }
-            result.add(s);
+            result.add(str);
         }  
         if(open<n){
-            dq.add('(');
-            backtrack(open+1, close, n, dq);
-            dq.removeLast();
+            backtrack(open+1, close, str+"(", n);
         }
         if(close<open){
-            dq.add(')');
-            backtrack(open, close+1, n, dq);
-            dq.removeLast();
+            backtrack(open, close+1, str+")",n);
         }
     }
     
